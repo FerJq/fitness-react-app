44a4f6738faa5b82831ec6d9620335104064215d
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-05-20 22:50:27 -0400

    API FIXED!

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react";
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
-import { exerciseOptions, fetchData } from "../utils/fetchData";
-import Exercises from "./Exercises";
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Exercises from './Exercises';
 
 const SearchExercises = () => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
 
   const handleSearch = async () => {
     if (search) {
       const exercisesData = await fetchData(
-        "https://exercisedb.p.rapidapi.com/exercises",
+        'https://exercisedb.p.rapidapi.com/exercises',
         exerciseOptions
       );
       console.log(exercisesData);
@@ -20,7 +20,7 @@ const SearchExercises = () => {
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography
         fontWeight={700}
-        sx={{ fontSize: { lg: "44px", xs: "30px" } }}
+        sx={{ fontSize: { lg: '44px', xs: '30px' } }}
         mb="50px"
         textAlign="center"
       >
@@ -30,32 +30,30 @@ const SearchExercises = () => {
         <TextField
           height="76px"
           sx={{
-            input: { fontWeight: "700", border: "none", borderRadius: "4px" },
-            width: { lg: "790px", xs: "350px" },
-            backgroundColor: "#fff",
-            borderRadius: "40px",
+            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+            width: { lg: '790px', xs: '350px' },
+            backgroundColor: '#fff',
+            borderRadius: '40px',
           }}
           value={search}
-          onChange={(e) => {
-            setSearch(e.target.value.toLowerCase());
-          }}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
           type="text"
         />
         <Button
           className="search-btn"
           sx={{
-            bgcolor: "#FF2625",
-            color: "#fff",
-            textTransform: "none",
+            bgcolor: '#FF2625',
+            color: '#fff',
+            textTransform: 'none',
             width: {
-              lg: "175px",
-              xs: "80px",
+              lg: '175px',
+              xs: '80px',
             },
-            fontSize: { lg: "20px", xs: "14px" },
-            height: "56px",
-            position: "absolute",
-            right: "0",
+            fontSize: { lg: '20px', xs: '14px' },
+            height: '56px',
+            position: 'absolute',
+            right: '0',
           }}
           onClick={handleSearch}
         >
