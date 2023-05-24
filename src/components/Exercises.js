3376437a4502553bc8fd07630b3a5338a942cc5f
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-05-24 18:56:17 -0400

    laying out the exercise card

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -4,23 +4,23 @@ import { Typography, Box, Stack } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
-
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   console.log(exercises);
   return (
-    <Box id={exercises} sx={{ mt: { lg: '110px' } }} mt="50px" p="20px">
+    <Box id="exercises" sx={{ mt: { lg: '110px' } }} mt="50px" p="20px">
       <Typography variant="h3" mb="46px">
-        <Stack
-          direction="row"
-          sx={{ gap: { lg: '110px', xs: '50px' } }}
-          flexWrap="wrap"
-          justifyContent="center"
-        >
-          {exercises.map((exercise, index) => (
-            <ExerciseCard key={index} exercise={exercise}/>
-          ))}
-        </Stack>
+        Showing Results
       </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: '110px', xs: '50px' }}}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
     </Box>
   );
 };
