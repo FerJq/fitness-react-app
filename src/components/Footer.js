2484661f15078101f1792040b7187ef34d5b3d0f
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-06-01 18:42:38 -0400

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
-import React from "react";
-
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo1.png';
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box mt="80x" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5" pb="40px" mt="20px">
+          Made with love by FjS.
+        </Typography>
+      </Stack>
+    </Box>
+  );
 };
 
 export default Footer;
