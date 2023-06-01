098ca2bd4f45e55117adcc8529f1b2462869bbf1
Fernanda Santiago <126302626+FerJq@users.noreply.github.com>
2023-06-01 19:24:19 -0400

    cleaning code removing  components without use

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
 
 import Logo from "../assets/images/Logo.png";
-import { BorderBottom } from "@mui/icons-material";
+
 
 const Navbar = () => {
   return (
